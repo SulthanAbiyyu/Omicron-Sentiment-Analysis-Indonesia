609869930d5307bf2e22aeee708e959cc5a3e124
Sulthan Abiyyu <zblackjack980@gmail.com>
2022-01-18 18:02:10 +0700

    Minor fix: model path and import issue
    Co-authored-by: Aditya Rizky Ramadhan <adityarizky2040@gmail.com>

--- webdevelopment/app.py
+++ webdevelopment/app.py
@@ -1,12 +1,12 @@
-from flask import Flask,render_template,request
-from ModelPipeline import Modelpipeline
+from flask import Flask, render_template, request
+from ModelPipeline import ModelPipeline
 
-app = Flask(__name__,template_folder="templates")
+app = Flask(__name__, template_folder="templates")
 
-def process_ml(teks) :
-    from ModelPipeline import ModelPipeline
-    MODEL_PATH = "../model/omicron-sentiment-analysis-indo.h5"
-    TOKENIZER_PATH = "../model/tokenizer_without_stopwords.pkl"
+
+def process_ml(teks):
+    MODEL_PATH = "./model/omicron-sentiment-analysis-indo.h5"
+    TOKENIZER_PATH = "./model/tokenizer_without_stopwords.pkl"
     MAX_LENGTH = 35
     mp = ModelPipeline(MODEL_PATH, TOKENIZER_PATH, MAX_LENGTH)
     prediction = mp.predict(teks)
@@ -18,13 +18,13 @@ def process_ml(teks) :
         return "Negative"
 
 
-@app.route('/',methods=['GET','POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
     answer = "None"
-    if request.method == 'POST' :
+    if request.method == 'POST':
         input_teks = request.form['teksmasuk']
         answer = process_ml(input_teks)
-    return render_template("index.html", p = answer)
+    return render_template("index.html", p=answer)
 
 
 if __name__ == '__main__':
