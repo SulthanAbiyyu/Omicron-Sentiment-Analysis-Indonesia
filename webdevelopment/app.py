f4dcfe5fb38896d2fdc77a81977888fffec50d09
Aditya Rizky Ramadhan <90701908+adityarizkyramadhan@users.noreply.github.com>
2022-01-25 08:03:15 +0700

    add time

--- webdevelopment/app.py
+++ webdevelopment/app.py
@@ -1,12 +1,13 @@
-from flask import Flask,render_template,request
+from flask import Flask, render_template, request
 from ModelPipeline import ModelPipeline
+import time
 
-app = Flask(__name__,template_folder="templates")
+app = Flask(__name__, template_folder="templates")
 
-def process_ml(teks) :
 
-    MODEL_PATH = "../model/omicron-sentiment-analysis-indo.h5"
-    TOKENIZER_PATH = "../model/tokenizer_without_stopwords.pkl"
+def process_ml(teks):
+    MODEL_PATH = "./model/omicron-sentiment-analysis-indo.h5"
+    TOKENIZER_PATH = "./model/tokenizer_without_stopwords.pkl"
     MAX_LENGTH = 35
     mp = ModelPipeline(MODEL_PATH, TOKENIZER_PATH, MAX_LENGTH)
     prediction = mp.predict(teks)
@@ -18,14 +19,19 @@ def process_ml(teks) :
         return "Negative"
 
 
-@app.route('/',methods=['GET','POST'])
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    answer = "None"
-    if request.method == 'POST' :
-        input_teks = request.form['teksmasuk']
+    time_now = time.time()
+    answer = "Belum Ada"
+    if request.method == 'POST':
+        input_teks = request.form['input']
         answer = process_ml(input_teks)
-    return render_template("index.html", p = answer)
+    time_end = time.time() - time_now
+    time_end = '{:.3f}'.format(time_end)    
+    return render_template("index.html", p=answer, t=time_end)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
